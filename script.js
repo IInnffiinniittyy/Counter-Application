4108c28b8d035fc9c23d1706bdb5a5ec5a70be12
IInnffiinniittyy <tanuj813@gmail.com>
2023-02-24 16:01:48 +0530

    Final Submission

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+// script for counter logic
+
 let display = document.querySelector("#display");
 
 let value = 0;
 
-document.querySelector("#decrement").addEventListener("click", () => { 
-    value--;
-    display.innerHTML = value;
+document.querySelector("#decrement").addEventListener("click", () => {
+  value--;
+  display.innerHTML = value;
 });
 
-document.querySelector("#reset").addEventListener("click", () => { 
-    value = 0;
-    display.innerHTML = value;
+document.querySelector("#reset").addEventListener("click", () => {
+  value = 0;
+  display.innerHTML = value;
 });
 
-document.querySelector("#increment").addEventListener("click", () => { 
-    value++;
-    display.innerHTML = value;
+document.querySelector("#increment").addEventListener("click", () => {
+  value++;
+  display.innerHTML = value;
+});
+
+// script for the toggling
+
+let toggle = document.querySelector("#toggle");
+let body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  this.classList.toggle("bi-moon");
+  if (this.classList.toggle("bi-brightness-high-fill")) {
+    body.style.background = "rgb(255, 132, 100)";
+    body.style.color = "black";
+    body.style.transition = "2s";
+  } else {
+    body.style.background = "black";
+    body.style.color = "white";
+    body.style.transition = "2s";
+  }
 });
